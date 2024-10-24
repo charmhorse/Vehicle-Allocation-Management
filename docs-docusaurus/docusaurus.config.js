09948ffc527ee2055a0ad6080ae80cdd9ce7e6b5
charmhorse <lassenygaard.dev@gmail.com>
2024-10-25 02:27:09 +0600

    ✨ docs: Update site URL in docusaurus.config.js

--- docs-docusaurus/docusaurus.config.js
+++ docs-docusaurus/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://fahimfba.github.io/vallocation',
+  url: 'https://fahimfba.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/vallocation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
