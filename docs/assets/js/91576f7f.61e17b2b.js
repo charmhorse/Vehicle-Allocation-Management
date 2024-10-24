d316ac42919823a86b31bc6768b8bc5b5f088648
charmhorse <lassenygaard.dev@gmail.com>
2024-10-25 02:31:50 +0600

    ⭕ Update static site

---
+++ docs/assets/js/91576f7f.61e17b2b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[450],{8281:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/vallocation/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
